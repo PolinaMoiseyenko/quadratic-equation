132cb60e672062afe14f8e1ac48e74c3e91ba9d9
PolinaMoiseyenko <pmoiseyenko@gmail.com>
2018-09-29 14:42:54 +0300

    Quadratic-equation

--- src/index.js
+++ src/index.js
@@ -1,13 +1,18 @@
 module.exports = function solveEquation(equation) {
   // your implementation
-const newEq = equation.split(' ').join(''),
-      equationArray = newEq.replace('x^2', '*').replace('x', '*').split('*'),
-      discriminant = equationArray[2]* equationArray[2] - 4 * equationArray[0] * equationArray[4],
+let newEq = equation.split(' ').join(''),
+    equationArray = newEq.replace('x^2', '*').replace('x', '*').split('*');
+if (equationArray[0] < 0 ) {
+  equationArray[0] *= (-1);
+  equationArray[2] *= (-1);
+  equationArray[4] *= (-1);
+};
+const discriminant = equationArray[2]* equationArray[2] - 4 * equationArray[0] * equationArray[4],
       sqrtDiscriminant = Math.sqrt(Math.abs(discriminant)),
       x1 = (-equationArray[2] - sqrtDiscriminant)/(2 * equationArray[0]),
       x2 = (-equationArray[2] + sqrtDiscriminant)/(2 * equationArray[0]);
 let newArr = [Math.round(parseFloat(x1)), Math.round(parseFloat(x2))];
-if (equationArray[0] < 0 )
+if (equationArray[0] < 0)
 return newArr.sort();
 else return newArr;
 }
